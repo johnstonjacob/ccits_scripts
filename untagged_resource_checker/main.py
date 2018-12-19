dc209f137244209880742370097184f8fb172340
Jacob Johnston <jacob@johnstonjacob.com>
2018-12-19 14:39:47 -0600

    add notifitcation to slack of untagged resources

--- untagged_resource_checker/main.py
+++ untagged_resource_checker/main.py
@@ -1,5 +1,10 @@
 import boto3
+from requests import post 
+from os import environ
 
+slack_webhook = environ["SLACK_WEBHOOK"]
+
+# accept input / pull list of accepted tags from elsewhere
 tag_keys = ['ce_name', 'test_tag']
 
 print(tag_keys)
@@ -20,7 +25,17 @@ def get_untagged_instances_by_region(region):
             print(i.instance_id)
             untagged_instances.append(i.instance_id)
 
+    #TODO: add region name to returned data to enable termination + easier finding and tagging of resource
     return untagged_instances
+
+def notify_slack(instance_ids):
+    instance_ids = '\n'.join(instance_ids)
+    payload = f"{{'text' : 'The following instance_ids are untagged: ```{instance_ids}```'}}"
+    r = post(slack_webhook, data=payload, headers={'Content-Type': 'application/json'})
+
+    if r.status_code != 200:
+        print(f'Slack webhook request error code {r.status_code}, the response is {r.text}')
+
 
 def main():
     regions = get_regions()
@@ -30,7 +45,9 @@ def main():
         print(f'Checking {r} for untagged instances..')
         untagged_instances += get_untagged_instances_by_region(r)
 
+    # ask what to do with these instances (notify, terminate, etc)
     print(untagged_instances)
+    notify_slack(untagged_instances)
     print("Done")
 
 if __name__ == "__main__":

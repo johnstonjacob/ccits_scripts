ba722baa8ee05568363cbf7bce31537d928f44ff
Jacob Johnston <jacob@johnstonjacob.com>
2018-12-19 14:13:13 -0600

    return list of untagged instance ids

--- untagged_resource_checker/main.py
+++ untagged_resource_checker/main.py
@@ -13,17 +13,24 @@ def get_regions():
 def get_untagged_instances_by_region(region):
     instances = [i for i in boto3.resource('ec2', region_name=region).instances.all()]
 
+    untagged_instances = []
+
     for i in instances:
         if not any(key in tag_keys for key in [t['Key'] for t in i.tags]):
             print(i.instance_id)
+            untagged_instances.append(i.instance_id)
+
+    return untagged_instances
 
 def main():
     regions = get_regions()
+    untagged_instances = []
 
     for r in regions:
         print(f'Checking {r} for untagged instances..')
-        get_untagged_instances_by_region(r)
+        untagged_instances += get_untagged_instances_by_region(r)
 
+    print(untagged_instances)
     print("Done")
 
 if __name__ == "__main__":
